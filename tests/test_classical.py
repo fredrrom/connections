443f7f58c8bf76c1ca5b2ee148908d806ca4c219
fredrik <fr409@cam.ac.uk>
2023-01-16 23:53:57 +0000

    Fixed tests

--- tests/test_classical.py
+++ tests/test_classical.py
@@ -13,7 +13,7 @@ class TestClassicalState:
         assert isinstance(self.state, ConnectionState)
         assert isinstance(self.state.matrix, Matrix)
         assert isinstance(self.state.goal.literal, Literal)
-        assert str([child.literal for child in self.state.tableau.children]) == '[big_f(_28061), big_f(f(_28061))]'
+        assert str([child.literal for child in self.state.tableau.children]) == '[big_f(_34251), big_f(f(_34251))]'
         assert str(self.state.tableau.children[0].literal) == str(self.state.goal.literal)
         assert self.state.tableau.children[0].children == []
         assert self.state.goal.depth == 1
@@ -24,11 +24,11 @@ class TestClassicalState:
         neg_lits = self.state.matrix.complements(self.state.goal.literal)
 
         # ASSERT
-        assert str(self.state.goal.literal) == 'big_f(_28061)'
+        assert str(self.state.goal.literal) == 'big_f(_34251)'
         assert neg_lits == [(0, 1), (2, 0), (2, 1)]
-        assert str(self.state._extensions()) == '[ex0: big_f(_28061) -> [big_f(_34365), -big_f(f(_34365))],' \
-                                                ' ex1: big_f(_28061) -> [-big_f(f(_28066)), -big_f(_28066)],' \
-                                                ' ex2: big_f(_28061) -> [-big_f(f(_28067)), -big_f(_28067)]]'
+        assert str(self.state._extensions()) == '[ex0: big_f(_34251) -> [big_f(_35885), -big_f(f(_35885))],' \
+                                                ' ex1: big_f(_34251) -> [-big_f(_34256), -big_f(f(_34256))],' \
+                                                ' ex2: big_f(_34251) -> [-big_f(_34257), -big_f(f(_34257))]]'
 
 
 class TestConnectionEnv:
@@ -44,10 +44,10 @@ class TestConnectionEnv:
         observation, reward, done, info = self.env.step(action)
 
         # ASSERT
-        assert str(action) == 'ex0: big_f(_28061) -> [big_f(_34362), -big_f(f(_34362))]'
+        assert str(action) == 'ex0: big_f(_34251) -> [big_f(_35882), -big_f(f(_35882))]'
         assert observation.max_depth == 2
         assert observation.goal.depth == 1
-        assert str(self.env.action_space) == '[ex1: big_f(_28061) -> [-big_f(f(_28063)), -big_f(_28063)],' \
-                                             ' ex2: big_f(_28061) -> [-big_f(f(_28064)), -big_f(_28064)]]'
+        assert str(self.env.action_space) == '[ex1: big_f(_34251) -> [-big_f(_34253), -big_f(f(_34253))],' \
+                                             ' ex2: big_f(_34251) -> [-big_f(_34254), -big_f(f(_34254))]]'
         assert observation.substitution == {}
         assert observation.proof_sequence == []
